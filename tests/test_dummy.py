aeec17d73b15f841504eac9857b6e226adbd49a0
netotz <neto.otz@hotmail.com>
2020-03-24 16:59:35 -0600

    Add dummy test
    This file only exists to prevent GitHub Actions to fail

---
+++ tests/test_dummy.py
@@ -0,0 +1,8 @@
+'''
+Dummy test file to prevent GitHub Actions to fail. This test have no practical use.
+
+This file can be deleted if there exists an actually useful test.
+'''
+
+def test_dummy():
+    '''Dummy test'''
