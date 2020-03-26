fca0737a4c16b789fcfbbb9844d67e66ae7cf5ed
netotz <neto.otz@hotmail.com>
2020-03-26 14:46:01 -0600

    Add function to parse arguments from command line

--- generator/cl_argsparse.py
+++ generator/cl_argsparse.py
@@ -5,3 +5,86 @@ Module to parse arguments from the command line.
 
 The parsed arguments are then used in other module to generate an instance.
 '''
+
+import sys
+import argparse
+from typing import Tuple
+
+def parse_arguments() -> Tuple[int, int, Tuple[int, int]]:
+    '''
+    An ArgumentParser object receives arguments from the command line
+    and returns them in a tuple of 3 elements.
+
+    (n: int, p: int, dimensions: Tuple[int, int])
+
+    If no arguments are given the program will end.
+    '''
+    # parser's description display when --help is used
+    description = 'Generates an random instance with the following arguments:'
+    # instantiate argument parser
+    parser = argparse.ArgumentParser(description=description)
+
+    # remove optional arguments from parser but store it to append it at the end
+    #* this 'hack' is used because otherwise the
+    #* required exclusive arguments are displayed as optional when they aren't
+    optional = parser._action_groups.pop()
+
+    # required arguments
+    # both 'n' and 'p' must be specified
+    required = parser.add_argument_group('requiered arguments')
+    required.add_argument(
+        'n',
+        type=int,
+        help='total number of candidate points'
+    )
+    required.add_argument(
+        'p',
+        type=int,
+        help='number of points to select'
+    )
+
+    # required exclusive arguments
+    # only one argument needs to be specified
+    shape = required.add_mutually_exclusive_group(required=True)
+    shape.add_argument(
+        '-s', '--square',
+        metavar='length',
+        type=int,
+        help='locate the n points in a squared plane of dimensions length * length at most'
+    )
+    shape.add_argument(
+        '-r', '--rectangle',
+        metavar=('length', 'width'),
+        nargs=2,
+        type=int,
+        help='locate the n points in a rectangular plane of dimensions length * width at most'
+    )
+
+    # optional arguments
+    optional.add_argument(
+        '-v', '--verbose',
+        type=int,
+        default=0,
+        choices=(1, 2),
+        help='increase output verbosity'
+    )
+    # append optional arguments to parser to display at the end
+    parser._action_groups.append(optional)
+
+    # if no arguments are given, display help as if -h was used
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    # parse arguments from command line
+    arguments = parser.parse_args()
+
+    # if chosen shape is squared
+    if arguments.square:
+        dimensions = (arguments.square, arguments.square)
+    # if chosen shape is rectangular
+    else:
+        dimensions = tuple(arguments.rectangle)
+
+    # return parsed arguments gathered in a tuple
+    return (arguments.n, arguments.p, dimensions)
