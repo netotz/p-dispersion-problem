35b8a2f3242790d899f9511e1a32b9b26bf19be3
netotz <neto.otz@hotmail.com>
2020-03-28 11:04:49 -0600

    Add models' package level imports

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,7 @@
 Package that contains the models of the project.
 These models are the classes of Point and PDPInstance.
 '''
+
+# package level imports
+from point import Point
+from pdp_instance import PDPInstance, Matrix
