253a46810042121f71c38e5de7ec27981c3eb893
netotz <neto.otz@hotmail.com>
2020-03-29 16:51:07 -0600

    Add method to write an instance to file

--- file_handling/file_io.py
+++ file_handling/file_io.py
@@ -1,3 +1,15 @@
 '''
 Module for writing and reading instances to and from files.
 '''
+
+from models import PDPInstance
+from .path import generate_filename, get_filepath
+
+def write_instance(instance: PDPInstance):
+    '''
+    Receives a PDP Instance and writes it to a file.
+    '''
+    filename = generate_filename(instance.n, instance.p)
+    filepath = get_filepath(filename)
+    with open(filepath, 'w') as file:
+        file.write(str(instance))
