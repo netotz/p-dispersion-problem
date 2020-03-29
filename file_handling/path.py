158ef6f1009fa628896e1e3826fa18d6ecbe2b7d
netotz <neto.otz@hotmail.com>
2020-03-29 14:53:08 -0600

    Add function to generate an instance's filename

--- file_handling/path.py
+++ file_handling/path.py
@@ -3,3 +3,11 @@ Module for handling directories and getting paths.
 '''
 
 import os
+
+def generate_filename(n: int, p: int, index: int = 0) -> str:
+    '''
+    Generates a name for an instance's file:
+
+    <n>_<p>_<index>.dat
+    '''
+    return str(n) + '_' + str(p) + '_' + str(index) + '.dat'
