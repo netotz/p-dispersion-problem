57bf06fb3fe7b6fa56b24262b8e37d3cbc5a2c50
netotz <neto.otz@hotmail.com>
2020-03-29 15:11:50 -0600

    Add function to get instance file's path

--- file_handling/path.py
+++ file_handling/path.py
@@ -11,3 +11,11 @@ def generate_filename(n: int, p: int, index: int = 0) -> str:
     <n>_<p>_<index>.dat
     '''
     return str(n) + '_' + str(p) + '_' + str(index) + '.dat'
+
+def get_filepath(filename: str) -> str:
+    '''
+    Returns the path of a file based on it's name.
+    '''
+    current_dir = os.path.dirname(__file__)
+    filepath = os.path.join(current_dir, '..', 'instances', filename)
+    return os.path.abspath(filepath)
