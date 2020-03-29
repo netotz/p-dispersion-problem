306f8c20406e7edbb592517d5e0ba68d35709f37
netotz <neto.otz@hotmail.com>
2020-03-29 16:38:07 -0600

    Test string representation of Point

--- tests/test_point.py
+++ tests/test_point.py
@@ -13,3 +13,10 @@ def test_distance():
     p1 = Point(0, 1, 2)
     p2 = Point(1, 3, 4)
     assert p1.distance(p2) == math.sqrt(abs(3 - 1) ** 2 + abs(4 - 2) ** 2)
+
+def test_str():
+    '''
+    Test string representation of a Point.
+    '''
+    p = Point(0, 1, 2)
+    assert str(p) == '0 1 2'
