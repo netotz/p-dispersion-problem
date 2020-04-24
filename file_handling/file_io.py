671de49a012ae2880cfecd02312c17948a1d02b7
netotz <neto.otz@hotmail.com>
2020-04-24 14:43:29 -0500

    Improve instance writing to file

--- file_handling/file_io.py
+++ file_handling/file_io.py
@@ -2,6 +2,8 @@
 Module for writing and reading instances to and from files.
 '''
 
+import os
+
 from models import PDPInstance
 from .path import generate_filename, get_filepath
 
@@ -9,7 +11,21 @@ def write_instance(instance: PDPInstance):
     '''
     Receives a PDP Instance and writes it to a file.
     '''
-    filename = generate_filename(instance.n, instance.p)
-    filepath = get_filepath(filename)
-    with open(filepath, 'w') as file:
-        file.write(str(instance))
+    index = 0
+    while True:
+        filename = generate_filename(instance.n, instance.p, index)
+        filepath = get_filepath(filename)
+        if os.path.exists(filepath):
+            index += 1
+        else:
+            break
+
+    folder = get_filepath('')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    try:
+        with open(filepath, 'w') as file:
+            file.write(str(instance))
+    except (IOError, OSError) as error:
+        print('The instance could not be written:\n', error)
