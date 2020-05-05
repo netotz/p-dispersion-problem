0ad6ffa37a063f4f826e0c714214eb631148dc85
netotz <neto.otz@hotmail.com>
2020-05-05 17:19:02 -0500

    Add simple output to test solver

--- solver/solve_instance.py
+++ solver/solve_instance.py
@@ -5,6 +5,8 @@ Module to solve a PDP instance.
 from typing import Tuple
 
 from file_handling import list_files, read_instance
+from heuristic.constructive import greedy_construction
+from heuristic.functions import objective_function
 
 def solve_instance(size: int, number: int, heuristics: Tuple[int, int], verbose: int):
     '''
@@ -21,4 +23,11 @@ def solve_instance(size: int, number: int, heuristics: Tuple[int, int], verbose:
     '''
     pdp_instances = (read_instance(file) for file in list_files(size, number))
     for instance in pdp_instances:
-        pass
+        print('Solving instance...')
+        solution = greedy_construction(instance)
+
+        #* temporary simple output, will be changed
+        print(solution)
+        f = objective_function(solution, instance.distances)
+        print(f'Obj func = {f}')
+        print()
